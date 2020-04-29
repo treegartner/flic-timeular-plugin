6b15a1b9aa519744076e8d8966f5ea93e23c7c3c
Christian <christian@nimbusec.com>
2020-04-29 18:41:26 +0200

    fix path in setup to change permissions

--- timeularPlugin/setup.sh
+++ timeularPlugin/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WORKINGDIR=$( dirname "${BASH_SOURCE[0]}" )
 test=$("$WORKINGDIR/flic-timeular-plugin" -config "$WORKINGDIR/config.toml" -action setup)
-chmod +x *.sh
+chmod +x $WORKINGDIR/*.sh
